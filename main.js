3ff9e9c2b6cc7edadbc4cf5d6df660c006417670
YamOhana <yamoh1993@gmail.com>
2020-02-03 17:48:49 +0200

    final changes

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-// $("#features").toggle(display)
+//----------------------------------------------slide example-----------------------------------------
 
+
+$("h1").on("click", function(){
+
+
+    $("#features").slideToggle(3000)
+
+
+})
 
 
 
@@ -8,8 +16,43 @@
 //     $("#example1").animate({ left: '250px' });
 
 // });
+//------------------------------------------ multiple function + finish + queue animated example---------------------------
 
-//------------------------------------------ multiple function animated example---------------------------
+//     $("#button2").click(function(){
+//       let div = $("#example2");
+//       startAnimation();
+//       function startAnimation(){
+//         div.animate({height: 300}, "slow")
+//         .animate({width: 300}, "slow")
+//         .css("background-color", "red") 
+//         .animate({height: 100}, "slow")
+//         .animate({width: 100}, "slow"/*, startAnimation*/);
+//       } 
+//     });
+
+//     $("#button3").click(function(){
+
+//     $("#example2").stop()
+
+
+//     // $("#example2").finish()
+
+//     showIt()
+
+
+//     })
+
+
+//   function showIt() {
+//     let n = $("#example2").queue( "fx" ); 
+//         console.log(n);
+        
+    
+//   }
+
+
+
+//------------------------------------------ multiple function animated example 2---------------------------
 
 
 // $("#button1").click(function () {
@@ -25,11 +68,38 @@
 
 //------------------------------------------ fade example-------------------------------------------------
 
-$(document).ready(function(){
-    $("#hide").click(function(){
-      $("p").hide();
-    });
-    $("#show").click(function(){
-      $("p").show();
-    });
-  });
+// $(document).ready(function(){
+//     $("#hide").click(function(){
+//       $("p").hide();
+//     });
+//     $("#show").click(function(){
+//       $("p").show();
+//     });
+//   });
+
+
+//------------------------------------------ toggle example-------------------------------------------------
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//       $("p").toggle();
+//     });
+//   });
+
+
+//------------------------------------------ progress bar example-------------------------------------------------
+
+
+// $(document).ready(function(){
+//     $("#btn").click(function(){
+//       $("#boxbar").animate({
+//         width: "400px"
+//       }, {
+//         duration: 5000,
+//         easing: "linear",
+//         step: function(x) {
+//           $("#demo").text(Math.round(x * 100 / 400)  + "%");  
+//         }
+//       });
+//     });
+//   });
